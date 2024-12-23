ae2645cf8b1f03fbacf10f08cea5ab1906ceb840
GBucho <161348938+GBucho@users.noreply.github.com>
2024-12-23 19:00:45 +0400

    supabase blogs creation

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -20,11 +20,20 @@ import { Input } from "../components/ui/input";
 import { supabase } from "../supabase";
 import { Controller, useForm } from "react-hook-form";
 import { userAtom } from "../store/auth";
+import { useEffect, useState } from "react";
 
 type BlogsListCreateValues = {
   title: string;
   description: string;
   image_file: null | File;
+};
+
+type SingleBlog = {
+  title: string;
+  description: string;
+  id: number;
+  image_url: string;
+  user_id: string;
 };
 
 const BlogsListFilterDefaultValues = {
@@ -35,9 +44,23 @@ const BlogsListFilterDefaultValues = {
 
 const Write = () => {
   const [user] = useAtom(userAtom);
+
+  const [blogs, setBlogs] = useState<SingleBlog>([]);
   const { control, handleSubmit } = useForm<BlogsListCreateValues>({
     defaultValues: BlogsListFilterDefaultValues,
   });
+
+  useEffect(() => {
+    supabase
+      .from("Blogs")
+      .select("*")
+      .throwOnError()
+      .then((res) => {
+        const blogsList = res.data as unknown as SingleBlog[];
+        setBlogs(blogsList);
+      });
+  }, []);
+
   const onSubmit = (formValues: BlogsListCreateValues) => {
     if (formValues?.image_file) {
       supabase.storage
@@ -57,59 +80,80 @@ const Write = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-y-4">
-      <div className="flex w-96 flex-col items-center gap-y-4 mt-2">
-        <Controller
-          name="title"
-          control={control}
-          render={({ field: { value, onChange } }) => {
-            return (
-              <Input
-                onChange={onChange}
-                value={value}
-                placeholder="Title"
-                className="border border-black"
-              />
-            );
-          }}
-        />
+    <div>
+      <div className="flex flex-col items-center justify-center gap-y-4">
+        <div className="flex w-96 flex-col items-center gap-y-4 mt-2">
+          <Controller
+            name="title"
+            control={control}
+            render={({ field: { value, onChange } }) => {
+              return (
+                <Input
+                  onChange={onChange}
+                  value={value}
+                  placeholder="Title"
+                  className="border border-black"
+                />
+              );
+            }}
+          />
 
-        <Controller
-          name="description"
-          control={control}
-          render={({ field: { value, onChange } }) => {
-            return (
-              <Input
-                onChange={onChange}
-                value={value}
-                placeholder="Description"
-                className="border border-black"
-              />
-            );
-          }}
-        />
+          <Controller
+            name="description"
+            control={control}
+            render={({ field: { value, onChange } }) => {
+              return (
+                <Input
+                  onChange={onChange}
+                  value={value}
+                  placeholder="Description"
+                  className="border border-black"
+                />
+              );
+            }}
+          />
 
-        <Controller
-          name="image_file"
-          control={control}
-          render={({ field: { onChange } }) => {
-            return (
-              <Input
-                onChange={(e) => {
-                  const file = e.target.files?.[0];
-                  onChange(file);
-                }}
-                type="file"
-                placeholder="File"
-                className="border border-black"
-              />
-            );
-          }}
-        />
+          <Controller
+            name="image_file"
+            control={control}
+            render={({ field: { onChange } }) => {
+              return (
+                <Input
+                  onChange={(e) => {
+                    const file = e.target.files?.[0];
+                    onChange(file);
+                  }}
+                  type="file"
+                  placeholder="File"
+                  className="border border-black"
+                />
+              );
+            }}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit(onSubmit)}>
+          Creata Blog
+        </button>
       </div>
-      <button type="submit" onClick={handleSubmit(onSubmit)}>
-        Creata Blog
-      </button>
+      <div className="flex flex-col gap-y-10 px-32">
+        {blogs.map((blogs) => {
+          const blogImageUrl = blogs.image_url
+            ? `${import.meta.env.VITE_SUPABASE_BLOG_IMAGES_STORAGE_URL}/${blogs.image_url}`
+            : "";
+          return (
+            <div
+              key={blogs.id}
+              className="flex flex-col gap-y-4 p-6 border border-gray-400"
+            >
+              <div>
+                <img className="border border-black" src={blogImageUrl} />
+              </div>
+              <div>{blogs?.title}</div>
+              <div>{blogs?.description}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
